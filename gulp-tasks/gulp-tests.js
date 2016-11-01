bb56f693c71d57c8eece8728635bc36c583cfc7b
Matt English <menglis@rei.com>
2016-11-01 11:23:08 -0700

    Break down of gulp file into tasks

---
+++ gulp-tasks/gulp-tests.js
@@ -0,0 +1,20 @@
+var gulp = require( 'gulp' );
+
+gulp.task( 'js', [], () =>
+    browserify( PATHS.SRC + '/js/main.js' ).bundle()
+    .pipe( source( PATHS.SRC + '/js/main.js' ) )
+    .pipe( streamify( sourcemaps.init() ) )
+    .pipe( rename( {
+        dirname: '/',
+        basename: 'rei-cedar'
+    } ) )
+    .pipe( streamify( sourcemaps.write() ) )
+    .pipe( gulp.dest( PATHS.DIST ) )
+    .pipe( rename( {
+        dirname: '/',
+        basename: 'rei-cedar',
+        suffix: '.min'
+    } ) )
+    .pipe( streamify( uglify() ) )
+    .pipe( gulp.dest( PATHS.DIST ) )
+);
